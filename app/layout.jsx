ec9b45798265b36312cd74b60651bc7fbea2d509
HangenYuu <97870091+HangenYuu@users.noreply.github.com>
2023-06-30 22:24:28 +0800

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import '@styles/globals.css';
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+
+export const metadata = {
+    title: "Promptr",
+    description: "Discover & share the best prompts for writing, journaling, and more with your AI chatbot.",
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <Provider>
+                <div className="main">
+                    <div className="gradient" />
+                </div>
+
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
